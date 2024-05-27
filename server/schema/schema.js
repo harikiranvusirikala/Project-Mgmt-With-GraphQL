f8ef20ca77e489e36f26b143175359de20573fe0
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-05-27 11:19:27 +0530

    Add Project Type & Queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -7,6 +7,17 @@ const { GraphQLObjectType,
   GraphQLList,
 } = require('graphql')
 
+// Project Type
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  })
+})
+
 // Client Type
 const ClientType = new GraphQLObjectType({
   name: 'Client',
@@ -21,6 +32,19 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects
+      }
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find(project => project.id == args.id)
+      }
+    },
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
