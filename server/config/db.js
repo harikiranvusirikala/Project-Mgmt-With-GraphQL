d165581210253e493fdb6766764234d39ff09d57
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-05-28 09:47:32 +0530

    Add database connection

---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+const moongose = require("mongoose")
+
+const connectDB = async () => {
+  const conn = await moongose.connect(process.env.MONGO_URI)
+
+  console.log(`MongoDB Connected ${conn.connection.host}`.cyan.underline.bold)
+}
+
+module.exports = connectDB
