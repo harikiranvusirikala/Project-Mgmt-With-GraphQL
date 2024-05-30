2abc9913599febdf02fdb8cca9d16ab21afce5cc
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-05-30 14:42:24 +0530

    Add get all clients query

---
+++ client/src/queries/clientQueries.js
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client';
+
+
+const GET_CLIENTS = gql`
+  query getClinets {
+    clients {
+      id
+      name
+      email
+      phone
+    }
+  }
+`
+
+export { GET_CLIENTS }
