16c2a44bc4380ee7759358dfc623a64fb4cb6ee6
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-06-01 09:50:47 +0530

    Add NotFound page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 const cache = new InMemoryCache({ // To resolve the warning
   typePolicies: {
@@ -35,6 +36,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='*' element={<NotFound />} />
           </Routes>
         </div>
       </BrowserRouter>
